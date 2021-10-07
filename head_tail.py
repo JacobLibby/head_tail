6c20b6443a86310aab0d8faa9d3d9b962f8a17b6
Jacob Libby <69590611+JacobLibby@users.noreply.github.com>
2021-10-06 21:51:29 -0400

    Add files via upload

---
+++ head_tail.py
@@ -0,0 +1,26 @@
+# Change name for each file
+input_filename = "input_file.txt"
+input_file = open(input_filename, 'r')
+
+# store first line in var: first_line
+first_line = input_file.readline() 
+
+# store last line in var: last_line
+import os
+with open(input_filename, 'rb') as f:
+    f.seek(-2, os.SEEK_END)
+    while f.read(1) != b'\n':
+        f.seek(-2, os.SEEK_CUR)
+    last_line = f.readline().decode()
+
+# craete output file
+output_filename = "output_file.txt"
+output_file = open(output_filename,'w')
+
+# write head_tail to output file
+head_tail = [first_line, last_line]
+output_file.writelines(head_tail)
+
+# close pipes
+input_file.close()
+output_file.close()
