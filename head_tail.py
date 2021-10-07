97beaeccd64996c1c6c4b84cd5300cd940d3bdef
Jacob Libby <69590611+JacobLibby@users.noreply.github.com>
2021-10-06 23:54:36 -0400

    Update head_tail.py

--- head_tail.py
+++ head_tail.py
@@ -1,31 +1,36 @@
 # Change name for each file
 input_filename = "input_file.txt"
 
+# Open pipe for input file
+input_file = open(input_filename, 'r')
+
 # create output file
 output_filename = "output_file.txt"
-output_file = open(output_filename,'w')
+output_file = open(output_filename, 'w')
 
 # number of desired lines 
 number_of_head_lines = 3
 number_of_tail_lines = 1
 
+## HEAD LINES ##
+
 # create array of lines
-first_lines = []
+head_lines = []
 for current_line in range(number_of_head_lines):
-    first_lines.append(input_file.readline())
+    head_lines.append(input_file.readline())
 
 # store last line in var: last_line
-last_lines = []
+tail_lines = []
 
-# Open pipe for input file
+# reopening to ensure integrity of file
 with open(input_filename) as file:
          
     # read last number_of_tail_lines lines from back of file
     for line in (file.readlines() [-number_of_tail_lines:]):
-        last_lines.append(line)
+        tail_lines.append(line)
 
 # write head_tail to output file
-head_tail = [first_lines, last_lines]
+head_tail = [head_lines, tail_lines]
 for each in range(len(head_tail)):
     output_file.writelines(head_tail[each])
 
@@ -35,8 +40,8 @@ output_file.close()
 
 # console output
 print("Done. Copied a total of " + str(number_of_head_lines) + " head lines and " + str(number_of_tail_lines) + " tail lines: \n \n")
-for each in first_lines:
+for each in head_lines:
     print(each)
 print("-"*30)
-for each in last_lines:
+for each in tail_lines:
     print(each)
